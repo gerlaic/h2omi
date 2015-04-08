b5d0abf92cb6fd74dc91dc2cc4ac55ebcf718387
gerlaic <gb1701@126.com>
2015-04-08 00:33:17 -0700

    update bg photoes

--- js/main.js
+++ js/main.js
@@ -99,6 +99,15 @@ $("body.bg-slider").backstretch([
    "images/bg7.jpg",
    "images/bg8.jpg",
    "images/bg9.jpg",
+   "images/bg1.jpg",
+   "images/bg2.jpg",
+   "images/bg3.jpg",
+   "images/bg4.jpg",
+   "images/bg5.jpg",
+   "images/bg6.jpg",
+   "images/bg10.jpg",
+   "images/bg11.jpg",
+   "images/bg12.jpg",
 ], {duration: 5000, fade: 1000});
 							  
 
