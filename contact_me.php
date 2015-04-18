61d5d122f94383c11af4136b721f6f12748f32e2
gerlaic <gb1701@126.com>
2015-04-17 22:36:27 -0700

    contact me update
    already link to db
    not working for now

--- contact_me.php
+++ contact_me.php
@@ -3,6 +3,7 @@
 if(empty($_POST['name'])  		||
    empty($_POST['email']) 		||
    empty($_POST['message'])	||
+   empty($_POST['subject']) ||
    !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL))
    {
 	echo "No arguments Provided!";
@@ -10,9 +11,31 @@ if(empty($_POST['name'])  		||
    }
 	
 $name = $_POST['name'];
-$email_address = $_POST['email'];
+$email = $_POST['email'];
 $message = $_POST['message'];
-	
+$subject = $_POST['subject'];
+
+//db lines
+
+//connect to CN db
+$dsn = "mysql:host=localhost;dbname=a0208102102";
+$db = new PDO($dsn,'a0208102102','75623203');
+
+/*query code:
+INSERT INTO  `contacts` (  `uid` ,  `name` ,  `email` ,  `subject` ,  `message` ,  `date` ) 
+VALUES (
+NULL ,  'test2',  'test2',  'teste2',  'msgmsg', CURDATE( )
+);
+*/
+
+$db->query("SELECT * FROM 'INSERT INTO  `contacts` (  `uid` ,  `name` ,  `email` ,  `subject` ,  `message` ,  `date` ) 
+	VALUES (
+		NULL ,  '$name',  '$email',  '$subject',  '$message', CURDATE( ));");
+
+return true;
+
+
+/*
 // create email body and send it	
 $to = 'co2.gerlaic@gmail.com'; // PUT YOUR EMAIL ADDRESS HERE
 $email_subject = "H2OMI WEBSITE MAIL:  $name"; // EDIT THE EMAIL SUBJECT LINE HERE
@@ -21,4 +44,7 @@ $headers = "From: co2.gerlaic@gmail.com\n";
 $headers .= "Reply-To: $email_address";	
 mail($to,$email_subject,$email_body,$headers);
 return true;			
+*/
+
+
 ?>0
